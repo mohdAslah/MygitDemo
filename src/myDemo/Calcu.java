ab24108afeb829ae62068a091d5bbac9cc8d4988
mohdAslah <jmi.aslah@gmail.com>
2022-01-16 23:33:54 +0530

    first commit

---
+++ src/myDemo/Calcu.java
@@ -0,0 +1,18 @@
+package myDemo;
+
+public abstract class Calcu {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	public void Run()
+	
+	{
+		
+		System.out.println("Need to Run");
+	}
+	
+	public abstract void Slow();
+}
---
+++ src/myDemo/NormalCal.java
@@ -0,0 +1,21 @@
+package myDemo;
+
+public class NormalCal extends Calcu {
+
+	public static void main(String[] args) {
+		
+		NormalCal a= new NormalCal();
+		
+		a.Run();
+		a.Slow();
+
+		
+	}
+
+	
+	public void Slow() {
+		System.out.println("Need to Slow");
+	}
+
+	
+}
